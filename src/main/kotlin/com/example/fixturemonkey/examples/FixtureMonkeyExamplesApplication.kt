78f27b955939cd063073cb7f42149ea788dec12e
Kuyho Chung <kchung1995@gmail.com>
2023-12-10 03:14:37 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/fixturemonkey/examples/FixtureMonkeyExamplesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.fixturemonkey.examples
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FixtureMonkeyExamplesApplication
+
+fun main(args: Array<String>) {
+    runApplication<FixtureMonkeyExamplesApplication>(*args)
+}
